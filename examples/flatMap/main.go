fe86bfe2d6b5ba5a3a5d6fed6cb89fdc9635d4a8
Ian Hansen <ihansen@digitalocean.com>
2015-03-03 19:05:00 -0500

    adds basic flatMap example

---
+++ examples/flatMap/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+// example of a flatMap type pattern where a channel of channels is turned into
+// a single output channel
+func main() {
+	for i := range flatMap(gen()) {
+		fmt.Printf("%d\n", i)
+	}
+}
+
+func gen() chan chan int {
+	out := make(chan chan int)
+	go func() {
+		defer close(out)
+		out <- intRange()
+		out <- intRange()
+		out <- intRange()
+	}()
+	return out
+}
+
+func intRange() chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		out <- 1
+		out <- 2
+		out <- 3
+	}()
+	return out
+}
+
+func flatMap(in chan chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for c := range in {
+			for i := range c {
+				out <- i
+			}
+		}
+	}()
+	return out
+}
