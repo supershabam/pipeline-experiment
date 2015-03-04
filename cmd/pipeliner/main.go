05dbb12f08e867a0bbb4cf4005e7acb7e236717f
Ian Hansen <ian@supershabam.com>
2015-03-04 03:14:18 -0500

    writes scan as if using generate
    first pass at batch

--- cmd/pipeliner/main.go
+++ cmd/pipeliner/main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
+	"os"
+	"time"
+
+	"github.com/supershabam/pipeline"
 )
 
-const src = `
-package main
-
-import "fmt"
-
 func main() {
-	fmt.Println("go pipeline something else ya jerk")
-}
-`
-
-func main() {
-	err := ioutil.WriteFile("./pipelined.go", []byte(src), 0644)
+	file, err := os.Create("./pipeline_maxSizeBatchString.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = pipeline.RenderMaxSizeBatch(file, pipeline.MaxSizeBatchConfig{
+		Package:   "main",
+		FuncName:  "batchString",
+		Type:      "string",
+		Timestamp: time.Now().Format("Jan 2, 2006 at 3:04pm (MST)"),
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
