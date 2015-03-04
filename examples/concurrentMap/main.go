6be32757a7d05b678fceeef54cee1d722dfadf94
Ian Hansen <ihansen@digitalocean.com>
2015-03-04 13:41:51 -0500

    adds err chan example

--- examples/concurrentMap/main.go
+++ examples/concurrentMap/main.go
@@ -16,10 +16,20 @@ func main() {
 		for i := 0; i < 20; i++ {
 			input <- fmt.Sprintf("%d", i)
 		}
+		input <- "too long for mah function"
 	}()
 
-	for output := range concMap(fOfS, input) {
-		fmt.Printf("output: %s\n", output)
+	// for output := range concMap(20, fOfS, input) {
+	// fmt.Printf("output: %s\n", output)
+	// }
+
+	output, errc := concErrMap(20, fOfSErr, input)
+	for out := range output {
+		fmt.Printf("output: %s\n", out)
+	}
+	err := <-errc
+	if err != nil {
+		fmt.Print(err)
 	}
 }
 
@@ -28,13 +38,22 @@ func fOfS(in string) string {
 	return fmt.Sprintf("fOfS(%s)", in)
 }
 
-func concMap(fn func(string) string, in <-chan string) <-chan string {
+func fOfSErr(in string) (string, error) {
+	time.Sleep(100 * time.Millisecond)
+	if len(in) > 10 {
+		return "", fmt.Errorf("string too long!")
+	}
+	return fmt.Sprintf("fOfSErr(%s)", in), nil
+}
+
+//go:generate pipeliner map(func(string) string) concurrently as concMap into conc_map.go
+func concMap(concurrency int, fn func(string) string, in <-chan string) <-chan string {
 	out := make(chan string)
 	go func() {
 		defer close(out)
 		wg := sync.WaitGroup{}
-		for i := 0; i < 20; i++ {
-			wg.Add(1)
+		wg.Add(concurrency)
+		for i := 0; i < concurrency; i++ {
 			go func() {
 				defer wg.Done()
 				for item := range in {
@@ -45,4 +64,47 @@ func concMap(fn func(string) string, in <-chan string) <-chan string {
 		wg.Wait()
 	}()
 	return out
+}
+
+//go:generate pipeliner map(func(string) (string, error) concurrently as concErrMap into conc_err_map.go
+func concErrMap(concurrency int, fn func(string) (string, error), in <-chan string) (<-chan string, <-chan error) {
+	out := make(chan string)
+	errc := make(chan error, 1)
+	done := make(chan struct{})
+	once := sync.Once{}
+	go func() {
+		defer close(out)
+		var outerErr error
+		defer func() {
+			errc <- outerErr
+		}()
+		wg := sync.WaitGroup{}
+		wg.Add(concurrency)
+		for i := 0; i < concurrency; i++ {
+			go func() {
+				defer wg.Done()
+				for {
+					select {
+					case <-done:
+						return
+					case item, ok := <-in:
+						if !ok {
+							return // end of channel
+						}
+						t, err := fn(item)
+						if err != nil {
+							once.Do(func() {
+								outerErr = err
+								close(done)
+							})
+							return
+						}
+						out <- t
+					}
+				}
+			}()
+		}
+		wg.Wait()
+	}()
+	return out, errc
 }
